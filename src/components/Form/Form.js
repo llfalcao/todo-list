4c5fd40a4673f0cdc6a08e9727e73b3813db9b3c
Luiz Otavio Lima Falcao <luizotavio-falcao@hotmail.com>
2021-09-25 06:47:13 -0300

    Reorganize form content

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -12,40 +12,44 @@ const Form = function () {
           <input type="text" id="form-item--description" name="description" />
         </div>
 
-        <div class="form-item">
-          <label for="form-item--date">Due Date</label>
-          <input type="text" id="form-item--date" name="date" />
-        </div>
+        <div class="form-group">
+          <div class="form-item left">
+            <label for="form-item--date">Due Date</label>
+            <input type="text" id="form-item--date" name="date" />
+          </div>
 
-        <p>Priority</p>
-        <div class="form-item radio-wrapper">
-          <div class="radio-item">
-            <input
-              type="radio"
-              id="form-item--priority-low"
-              name="priority"
-              value="low"
-            />
-            <label for="form-item--priority-low">Low</label>
-          </div>
-          <div class="radio-item">
-            <input
-              type="radio"
-              id="form-item--priority-normal"
-              name="priority"
-              value="normal"
-              checked
-            />
-            <label for="form-item--priority-normal">Normal</label>
-          </div>
-          <div class="radio-item">
-            <input
-              type="radio"
-              id="form-item--priority-high"
-              name="priority"
-              value="high"
-            />
-            <label for="form-item--priority-high">High</label>
+          <div class="form-item right">
+            <p>Priority</p>
+            <div class="radio-wrapper">
+              <div class="radio-item">
+                <input
+                  type="radio"
+                  id="form-item--priority-low"
+                  name="priority"
+                  value="low"
+                />
+                <label for="form-item--priority-low">Low</label>
+              </div>
+              <div class="radio-item">
+                <input
+                  type="radio"
+                  id="form-item--priority-normal"
+                  name="priority"
+                  value="normal"
+                  checked
+                />
+                <label for="form-item--priority-normal">Normal</label>
+              </div>
+              <div class="radio-item">
+                <input
+                  type="radio"
+                  id="form-item--priority-high"
+                  name="priority"
+                  value="high"
+                />
+                <label for="form-item--priority-high">High</label>
+              </div>
+            </div>
           </div>
         </div>
 
