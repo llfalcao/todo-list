53e2c0c8eae56d37e0d079e0fb400b465b49d985
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-09-27 02:14:41 -0300

    Fix spacing on header buttons

--- src/sections/ProjectSection/ProjectSection.js
+++ src/sections/ProjectSection/ProjectSection.js
@@ -3,7 +3,16 @@ const Projects = function () {
     <nav id="projects">
       <header class="projects--header">
         <h2>Projects</h2>
-        <button id="btn-new-project" class="btn" type="button">+</button>
+        <button id="btn-new-project" class="btn" type="button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"></path>
+          </svg>
+        </button>
       </header>
 
       <section class="projects--list"></section>
